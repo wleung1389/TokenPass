c9a99ad086756b641811bfe88d1b006fe798856d
BT_1N3_16 <wleung1389@bths.edu>
2018-11-14 09:34:52 -0500

    hi

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,14 @@
 public class Runner {
     public static void main(String[] args)
     {
-        TokenPass t = new TokenPass(4);
-        t.print();
-        t.distributePlayerTokens();
-        t.print();
+        TokenPass game1 = new TokenPass(4);
+        while(game1.gameOver() < 0)
+        {
+            game1.printBoard();
+            game1.distributePlayerTokens();
+            game1.nextPlayer();
+        }
+        game1.printBoard();
+        System.out.println("Game Winner : Player " + game1.gameOver());
     }
 }
--- src/TokenPass.java
+++ src/TokenPass.java
@@ -1,26 +1,22 @@
 public class TokenPass {
     private int[] board;
     private int currentPlayer;
-    public TokenPass(int playerCount)
-    {
+
+    public TokenPass(int playerCount) {
         this.board = new int[playerCount];
-        for(int i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             board[i] = getRandomInteger(1, 10);
         }
-        currentPlayer = getRandomInteger(0,board.length - 1);
+        currentPlayer = getRandomInteger(0, board.length - 1);
     }
-    public void distributePlayerTokens()
-    {
+
+    public void distributePlayerTokens() {
         int totalTokens = board[currentPlayer];
         int pos = currentPlayer;
-        for(int i = totalTokens; i > 0; i--)
-        {
-            if(pos >= board.length - 1)
-            {
+        for (int i = totalTokens; i > 0; i--) {
+            if (pos >= board.length - 1) {
                 pos = 0;
-            }
-            else
-            {
+            } else {
                 pos++;
             }
             board[currentPlayer] -= 1;
@@ -28,17 +24,50 @@ public class TokenPass {
             board[pos] += 1;
         }
     }
-    public int getRandomInteger(int min, int max)
-    {
+
+    public int getRandomInteger(int min, int max) {
         return (int) Math.floor((Math.random() * (max - min + 1)) + min);
     }
-    public void print()
-    {
-        for(int i = 0; i < board.length; i++)
-        {
-            System.out.print(" | " + board[i]);
+
+    public void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("Player " + i + " : " + board[i] + "  ");
         }
-        System.out.print(" |");
-        System.out.println();
+        System.out.println("Current Player : " + currentPlayer);
+    }
+
+    public void nextPlayer()
+    {
+        if(currentPlayer >= board.length - 1)
+        {
+            currentPlayer = 0;
+
+        }
+        else
+        {
+            currentPlayer++;
+        }
+    }
+    public int gameOver()
+    {
+        int game = -1;
+        while(game == -1)
+        {
+            for(int i = 0; i < board.length; i++)
+            {
+                if(board[i] == 0)
+                {
+                    return i;
+                }
+                else
+                {
+                    if(i == board.length - 1)
+                    {
+                        game = -2;
+                    }
+                }
+            }
+        }
+        return game;
     }
 }
